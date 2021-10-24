733dcc50cdebbdc71880f0c71e9ad7e93f58d9d2
Barrakuda <brpslelush@gmail.com>
2021-10-24 18:45:14 +0200

    First Commit

---
+++ lol/urls.py
@@ -0,0 +1,34 @@
+"""lol URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from mainapp import views as mainapp
+
+urlpatterns = [
+    path('', mainapp.index, name="home"),
+    path('collection/', mainapp.collection, name="collection"),
+    path('loot/', mainapp.loot, name="loot"),
+    path('store/', mainapp.store, name="store"),
+    path('loot/chests/', mainapp.loot, name="loot_chests"),
+    path('loot/champs/', mainapp.loot, name="loot_champs"),
+    path('loot/skins/', mainapp.loot, name="loot_skins"),
+    path('loot/emotes/', mainapp.loot, name="loot_emotes"),
+    path('loot/wards/', mainapp.loot, name="loot_wards"),
+    path('loot/icons/', mainapp.loot, name="loot_icons"),
+
+    
+    path('admin/', admin.site.urls),
+]
